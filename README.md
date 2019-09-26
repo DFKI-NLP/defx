dca22db49ef1df5634ebbc8cbf781e26cd5f7e18
Marc <marchbnr@users.noreply.github.com>
2019-09-26 17:28:06 +0200

    Initial commit
     * Create editorconfig
     * Create README.md
     * Create .gitignore
     * Create LICENSE
     * Create basic module structure
     * Add a pylint config
     * Add a requirements file
     * Ignore the data folder

---
+++ README.md
@@ -0,0 +1,5 @@
+# Semeval 2020 Task 6
+DeftEval: Extracting term-definition pairs in free text
+
+## Requirements
+Tested with Python 3.7.4
---
+++ defx/__init__.py
@@ -0,0 +1,4 @@
+"""AllenNLP module for the DeftEval definition extraction task"""
+
+# pylint: disable=wildcard-import
+from defx.dataset_readers import *
